047511bf10eb03d9e1c0c8274c4426659fc5298d
salmaouirini <ouirinisalma1@gmail.com>
2024-10-14 14:52:09 -0400

    Set up database connectivity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:vBIWWcBNF9qrFiFsZaEP21Je@172.21.54.121:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
